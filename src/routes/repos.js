3f17a1a591e9f40e4b76e56ef6d239afd745bfd6
Simonetti <mateus.simonetti@upflow.me>
2021-02-04 16:04:18 -0300

    Voltando ao normal

--- src/routes/repos.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controller/repos");
-
-router.get("/fiveOldestCSharpRepos", controller.getRepos);
-
-module.exports = router;
